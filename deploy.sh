4fa499a6e15ea1340032f3e311c92f4fd0a42c4d
Pattisier <Principursa@protonmail.com>
2025-07-05 21:37:32 +0200

    minor contract changes

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+ forge script  script/PensionVault.s.sol --rpc-url  "https://worldchain-sepolia.g.alchemy.com/public" --broadcast -vvvv --private-key $PRIVATE_KEY --verify 
--- src/GeneticRegistry.sol
+++ src/GeneticRegistry.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract GeneticRegistry {
-    mapping ( bytes32 => bool ) geneticHashes;
+    mapping ( address id => mapping ( bytes32 genomeHash => bytes32 treeHash)) hashData;
 
-    event HashAddded(bytes32 Hash);
+    event HashAddded(address userId, bytes32 genomeHash, bytes32 treeHash);
+    event TreeHashUpdated(address userId, bytes32 treeHash);
 
-    function addToRegistry(bytes32 hash) public {
-      require(geneticHashes[hash] == false, "Hash already exists");
-      geneticHashes[hash] = true;
-      emit HashAddded(hash);
+    function addToRegistry(address userAddress, bytes32 genomeHash, bytes32 treeHash) public {
+      require(hashData[userAddress][genomeHash] == 0, "Genome data already exists for user");
+      hashData[userAddress][genomeHash] = treeHash;
+      emit HashAddded(userAddress, genomeHash, treeHash);
     }
-    function checkAncestry(bytes32 hash1, bytes32 hash2) public {
+    function checkAncestry(bytes32 hash1, bytes32 hash2,bytes32 proof) public {
+   }
 
+   //add user validation later
+   function updateTreeHash(address userAddress, bytes32 genomeHash, bytes32 treeHash) public {
+     require(hashData[userAddress][genomeHash] != 0,"treeHash does not exist yet");
+     hashData[userAddress][genomeHash] == treeHash;
+     emit TreeHashUpdated(userAddress,treeHash);
 
-    }
+   }
 } 
